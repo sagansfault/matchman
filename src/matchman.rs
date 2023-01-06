eb9617e38752dc8b7ea555a68c9a63d3371e38d1
sagan <camjanssen120@gmail.com>
2023-01-05 19:34:57 -0800

    change to in-rust memory instead of redis

--- src/matchman.rs
+++ src/matchman.rs
@@ -1,3 +1,12 @@
+pub mod data {
+    use dashmap::DashMap;
+
+    pub struct Data {
+        fights: DashMap<u64, Vec<super::fight::Fight>>,
+        queues: DashMap<u64, Vec<super::user::MMUser>>
+    }
+}
+
 pub mod user {
     pub struct MMUser {
         pub id: u64,
@@ -22,7 +31,6 @@ pub mod game {
 
 pub mod fight {
     use super::user::MMUser;
-
     pub struct Fight {
         pub p1: MMUser,
         pub p2: MMUser
